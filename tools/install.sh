1573abad1dff4decf6efd2dd9212562f9f917e87
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-07-03 21:24:55 +0700

    chore: update tools script

--- tools/install.sh
+++ tools/install.sh
@@ -4,3 +4,4 @@ echo "Installing tools..."
 go install github.com/cosmtrek/air@latest
 go install github.com/momaek/formattag@latest
 go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+go install github.com/swaggo/swag/cmd/swag@latest
