17fb7e819cac8cc2d9f540d06b6745e9565d8f48
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-07-11 20:56:20 +0700

    feat: implement user and account logic for google sign in

--- app/auth/repo.go
+++ app/auth/repo.go
@@ -1,1 +1,43 @@
 package auth
+
+import (
+	"context"
+	"errors"
+
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog/log"
+)
+
+var ErrUserDoesNotExist = errors.New("User does not exist")
+var ErrAccountDoesNotExist = errors.New("Account does not exist")
+
+func findUserByEmail(ctx context.Context, tx pgx.Tx, email string) (user User, err error) {
+	q := "SELECT * FROM users WHERE email = $1 AND deleted_at IS NULL"
+
+	if err = pgxscan.Get(ctx, tx, &user, q, email); err != nil {
+		if err.Error() == "scanning one: no rows in result set" {
+			return user, ErrUserDoesNotExist
+		}
+
+		log.Err(err).Msg("Failed to find user by email")
+		return user, err
+	}
+
+	return user, nil
+}
+
+func findAccountByProviderAndProviderAccountId(ctx context.Context, tx pgx.Tx, provider AccountProvider, providerAccountId string) (account Account, err error) {
+	q := "SELECT * FROM accounts WHERE provider = $1 AND provider_account_id = $2 AND deleted_at IS NULL"
+
+	if err = pgxscan.Get(ctx, tx, &account, q, provider, providerAccountId); err != nil {
+		if err.Error() == "scanning one: no rows in result set" {
+			return account, ErrAccountDoesNotExist
+		}
+
+		log.Err(err).Msg("Failed to find account by provider and provider account id")
+		return account, err
+	}
+
+	return account, nil
+}
--- app/auth/service.go
+++ app/auth/service.go
@@ -3,6 +3,9 @@ package auth
 import (
 	"context"
 	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"gopkg.in/guregu/null.v4"
 )
 
 func signInWithGoogle(ctx context.Context, idToken string) (signIn UserSignIn, errs map[string]error, err error) {
@@ -11,13 +14,68 @@ func signInWithGoogle(ctx context.Context, idToken string) (signIn UserSignIn, e
 		return
 	}
 
-	_, name, email, imageUrl := extractProfileFromGoogleIdTokenPayload(payload)
-	user, errs := NewUserWithOAuth(name, email, imageUrl)
-	if errs != nil {
+	accountId, name, email, imageUrl := extractProfileFromGoogleIdTokenPayload(payload)
+
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		log.Err(err).Msg("Failed to sign in with Google")
 		return
 	}
 
-	fmt.Println(user)
+	defer tx.Rollback(ctx)
+
+	var user User
+	var account Account
+
+	user, err = findUserByEmail(ctx, tx, email.String)
+	if err != nil {
+		if err != ErrUserDoesNotExist {
+			return
+		}
+
+		user, errs = NewUserWithOAuth(name, email, imageUrl)
+		if errs != nil {
+			return
+		}
+
+		account, err = findAccountByProviderAndProviderAccountId(ctx, tx, GOOGLE, accountId)
+		if err != nil {
+			if err != ErrAccountDoesNotExist {
+				return
+			}
+
+			account, errs = NewAccount(
+				user.Id.String(),
+				string(OAUTH),
+				null.NewString("", false),
+				string(GOOGLE),
+				accountId,
+			)
+			if errs != nil {
+				return
+			}
+		}
+	} else {
+		account, err = findAccountByProviderAndProviderAccountId(ctx, tx, GOOGLE, accountId)
+		if err != nil {
+			if err != ErrAccountDoesNotExist {
+				return
+			}
+
+			account, errs = NewAccount(
+				user.Id.String(),
+				string(OAUTH),
+				null.NewString("", false),
+				string(GOOGLE),
+				accountId,
+			)
+			if errs != nil {
+				return
+			}
+		}
+	}
+
+	fmt.Println(user, account)
 
 	return signIn, nil, nil
 }
