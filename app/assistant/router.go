35cd4544d63883a27e19ec59f297b6f772aeebbe
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2023-07-12 09:09:12 +0700

    fix: disable user auth

--- app/assistant/router.go
+++ app/assistant/router.go
@@ -2,13 +2,13 @@ package assistant
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/lexica-app/lexicapi/app/auth"
 )
 
 func Router() *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Use(auth.UserAuthMiddleware)
+	// TODO: uncomment if client has supported user auth
+	// r.Use(auth.UserAuthMiddleware)
 
 	r.Post("/simplify", simplifyTextHandler)
 	r.Post("/explain", explainTextHandler)
